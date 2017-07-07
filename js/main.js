cb61f63e61d8499cc91f0cc60bc0c81024844895
Michael Murphy <murphym18@gmail.com>
2017-07-07 14:58:16 -0700

    Add Split and combine functions

---
+++ js/main.js
@@ -0,0 +1,57 @@
+function splitStrings(str) {
+  return str.split(/[\.]\s+/g).map(function(s) {
+    return s.trim()
+  }).filter(function(s) {
+    return s !== ""
+  })
+}
+
+function combineStrings(strs) {
+  return strs.reduce(function(cur, next) {
+    next = next.trim()
+    var lastChar = next[next.length - 1];
+    if (lastChar !== ".") {
+      next = next + "."
+    }
+    return cur + " " + next
+  }, "").trim()
+}
+
+function makeTextAreaHtml(str) {
+  return `<div >
+      <textarea placeholder="Sentence" class="form-control a-sentence" rows="9">${str}</textarea>
+  </div><br>`
+}
+
+function getAllSentences() {
+  return $(".a-sentence").map(function(e, textArea) {
+    return textArea.value
+  }).toArray().filter(function(s) {
+    return s !== ""
+  })
+}
+
+function onCombineButton() {
+  // iterate through each text area and collect each string
+  // join all the strings together
+  // render one text area with this string
+  // replace the current html with this one
+  $('.all-sentences').html(makeTextAreaHtml(combineStrings(getAllSentences())))
+}
+
+function onSplitButton() {
+  // iterate through each text area and collect each string
+  // join all teh strings together
+  // then split the strings sentence by sentence
+  // then for each string render html for text area with the string as the value
+  // join all the rendered html together
+  // replace the current gui with this newly rendered html
+  var h = splitStrings(combineStrings(getAllSentences())).map(makeTextAreaHtml).join("")
+  $('.all-sentences').html(h)
+}
+
+$(function() {
+  $("#split_button").on('click', onSplitButton)
+  $("#combine_button").on('click', onCombineButton)
+  $('.all-sentences').html([""].map(makeTextAreaHtml).join(""))
+})
