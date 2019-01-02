3564142a19825f0d38e586682ff24ee332e6542c
Michael Murphy <murphym18@gmail.com>
2019-01-02 06:37:10 -0800

    Add AWS key placeholders to cred.py

--- cred.py
+++ cred.py
@@ -3,3 +3,9 @@ def get_ibm_username():
 
 def get_ibm_password():
     return 'PASSWORD HERE'
+
+def get_aws_key_id():
+    return 'AWS KEY ID HERE'
+
+def get_aws_secret_key():
+    return 'AWS SECRET KEY HERE'
